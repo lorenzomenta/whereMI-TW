8a0372e9be873391f1ecb6ffd2ba17731fde707d
Lorenzo Menta <lorenzo.menta@gmail.com>
2020-01-27 15:48:11 +0100

    update map browser

--- js/main.js
+++ js/main.js
@@ -34,3 +34,86 @@ $(function(){
         $('.back-to-top').removeClass('active-top');
     }
 });
+
+// ==== Map ====
+const MAP = 'pk.eyJ1IjoibG9yZW56b21lbnRhIiwiYSI6ImNrNGI3MnNwdTBheDUzbm9nanlsbmtmZHQifQ.Ag_McrzQN8_QGxI7Bo9QYg';
+var position;
+var positionCoord;
+var destination;
+var destinationCoord;
+var routeControl;
+let options = {profile: 'mapbox/walking', language: 'it'}
+
+
+var map = L.map('map').setView([44.505833, 11.341667], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoibG9yZW56b21lbnRhIiwiYSI6ImNrNGI3MXRjbjBhdTgzbW14NnpqcHgyczUifQ.IrmWvBDIlTuzSi6qCrVUvA'
+}).addTo(map);
+
+
+// ==== Icons ====
+
+var userIcon = L.icon({
+  iconUrl: 'images/street-view.svg',
+  iconSize:     [30, 60], // size of the icon
+  iconAnchor:   [15, 40], // point of the icon which will correspond to marker's location
+  popupAnchor:  [0, -40] // point from which the popup should open relative to the iconAnchor
+});
+
+var markerIcon = L.icon({
+  iconUrl: 'images/map-marker.svg',
+  iconSize:     [30, 60], // size of the icon
+  iconAnchor:   [15, 40], // point of the icon which will correspond to marker's location
+  popupAnchor:  [0, -40] // point from which the popup should open relative to the iconAnchor
+});
+
+for ( var i=0; i < markers.length; ++i )
+{
+   var markerLat = markers[i].lat;
+   var markerLng = markers[i].lng;
+   var marker = L.marker( [markers[i].lat, markers[i].lng] ,  {icon: markerIcon})
+      .bindPopup('<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' + "<br>" + markers[i].lat + "°, " + markers[i].lng + "° <br><br><hr><br>" + '<a href="#clip">' + "Vai alla Clip!" + '</a>' )
+      .addTo( map )
+      marker.on('click', function(marker){
+            destinationCoord = this.getLatLng();
+            routeControl.setWaypoints([
+                positionCoord,
+                destinationCoord
+              ])
+        });
+}
+// ==== Geolocation and Routing ====
+
+map.locate({setView: true, maxZoom: 13});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    position = L.marker(e.latlng, {icon : userIcon}).addTo(map)
+        .bindPopup("Ti trovi entro " + radius + " metri da questo punto").openPopup();
+
+    positionCoord = e.latlng;
+
+    console.log(positionCoord);
+
+    L.circle(e.latlng, radius).addTo(map);
+
+
+
+    routeControl =  L.Routing.control({
+          routeWhileDragging: true,
+          router: new L.Routing.mapbox(MAP, options)
+        }).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
---
+++ js/markers.js
@@ -0,0 +1,94 @@
+var markers = [
+  {
+     "name": "Piazza Maggiore",
+     "url": "https://it.wikipedia.org/wiki/Piazza_Maggiore",
+     "lat": 44.49369,
+     "lng": 11.34305,
+     "latLng" : '44.49369, 11.34305'
+  },
+  {
+     "name": "Basilica di San Petronio",
+     "url": "https://it.wikipedia.org/wiki/Basilica_di_San_Petronio",
+     "lat": 44.492778,
+     "lng": 11.343056
+  },
+  {
+     "name": "Le due Torri",
+     "url": "https://it.wikipedia.org/wiki/Torri_di_Bologna#Le_due_torri",
+     "lat": 44.494167,
+     "lng": 11.346667
+  },
+  {
+     "name": "Basilica di Santo Stefano",
+     "url": "https://it.wikipedia.org/wiki/Basilica_di_Santo_Stefano_(Bologna)",
+     "lat": 44.491985,
+     "lng": 11.348727
+  },
+  {
+     "name": "Archiginnasio",
+     "url": "https://it.wikipedia.org/wiki/Archiginnasio_di_Bologna",
+     "lat": 44.492174,
+     "lng": 11.343491
+  },
+  {
+     "name": "Finestrella",
+     "url": "https://it.wikipedia.org/wiki/Canali_di_Bologna",
+     "lat": 44.498576,
+     "lng": 11.345222
+  },
+  {
+     "name": "Pinacoteca",
+     "url": "https://it.wikipedia.org/wiki/Pinacoteca_nazionale_di_Bologna",
+     "lat": 44.4977,
+     "lng": 11.353469
+  },
+  {
+     "name": "Santuario della Madonna di San Luca",
+     "url": "https://it.wikipedia.org/wiki/Santuario_della_Madonna_di_San_Luca",
+     "lat": 44.479117,
+     "lng": 11.298069
+  },
+  {
+     "name": "Santuario di Santa Maria della Vita",
+     "url": "https://it.wikipedia.org/wiki/Santuario_di_Santa_Maria_della_Vita",
+     "lat": 44.493398,
+     "lng": 11.344617
+  },
+  {
+     "name": "Giardini Margherita",
+     "url": "https://it.wikipedia.org/wiki/Giardini_Margherita",
+     "lat": 44.48431,
+     "lng": 11.355896
+  },
+  {
+     "name": "MAMbo Museo di Arte Moderna",
+     "url": "https://it.wikipedia.org/wiki/Museo_d%27arte_moderna_di_Bologna",
+     "lat": 44.5025,
+     "lng": 11.3369
+  },
+  {
+     "name": "Cimitero monumentale della Certosa",
+     "url": "https://it.wikipedia.org/wiki/Cimitero_monumentale_della_Certosa_di_Bologna",
+     "lat": 44.496389,
+     "lng": 11.309444
+  },
+  {
+     "name": "Torre dei Prendiparte",
+     "url": "https://it.wikipedia.org/wiki/Torre_dei_Prendiparte",
+     "lat": 44.495975,
+     "lng": 11.344775
+  },
+  {
+     "name": "Palazzo di Re Enzo",
+     "url": "https://it.wikipedia.org/wiki/Palazzo_Re_Enzo",
+     "lat": 44.4943,
+     "lng": 11.343169
+  },
+  {
+     "name": "Basilica di San Francesco",
+     "url": "https://it.wikipedia.org/wiki/Basilica_di_San_Francesco_(Bologna)",
+     "lat": 44.495,
+     "lng": 11.335278
+  },
+
+];
