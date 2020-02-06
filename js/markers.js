ead1e083f24acd8e9aa7c41d2c9980f1d1bcedb9
Lorenzo Menta <lorenzo.menta@gmail.com>
2020-02-06 22:56:12 +0100

    clips txt

--- js/markers.js
+++ js/markers.js
@@ -1,90 +1,106 @@
 var markers = [
   {
      "name": "Piazza Maggiore",
+     "id" : "twKR6F6XFVUE",
      "url": "https://it.wikipedia.org/wiki/Piazza_Maggiore",
      "lat": 44.49369,
-     "lng": 11.34305
+     "lng": 11.34305,
   },
   {
      "name": "Basilica di San Petronio",
+     "id" : "tw1D36O6EAMK",
      "url": "https://it.wikipedia.org/wiki/Basilica_di_San_Petronio",
      "lat": 44.492778,
      "lng": 11.343056
   },
   {
      "name": "Le due Torri",
+     "id" : "tw0FWROW8IXO",
      "url": "https://it.wikipedia.org/wiki/Torri_di_Bologna#Le_due_torri",
      "lat": 44.494167,
      "lng": 11.346667
   },
   {
      "name": "Basilica di Santo Stefano",
+     "id" : "twVB8STGUY3W",
      "url": "https://it.wikipedia.org/wiki/Basilica_di_Santo_Stefano_(Bologna)",
      "lat": 44.491985,
      "lng": 11.348727
   },
   {
      "name": "Archiginnasio",
+     "id" : "twIOONR7BG1A",
      "url": "https://it.wikipedia.org/wiki/Archiginnasio_di_Bologna",
      "lat": 44.492174,
      "lng": 11.343491
   },
   {
      "name": "Finestrella",
+     "id" : "twALOQK1C6VJ",
      "url": "https://it.wikipedia.org/wiki/Canali_di_Bologna",
      "lat": 44.498576,
      "lng": 11.345222
   },
   {
      "name": "Pinacoteca",
+     "id" : "twLZJMVTHQ88",
      "url": "https://it.wikipedia.org/wiki/Pinacoteca_nazionale_di_Bologna",
      "lat": 44.4977,
      "lng": 11.353469
   },
   {
      "name": "Santuario della Madonna di San Luca",
+     "id" : "twRX5P3O5HZH",
      "url": "https://it.wikipedia.org/wiki/Santuario_della_Madonna_di_San_Luca",
      "lat": 44.479117,
      "lng": 11.298069
   },
   {
      "name": "Santuario di Santa Maria della Vita",
+     "id": "tw96YC5Z9G44",
      "url": "https://it.wikipedia.org/wiki/Santuario_di_Santa_Maria_della_Vita",
      "lat": 44.493398,
-     "lng": 11.344617
+     "lng": 11.344617,
+
   },
   {
      "name": "Giardini Margherita",
+     "id" : "twVL4KS2P1SU",
      "url": "https://it.wikipedia.org/wiki/Giardini_Margherita",
      "lat": 44.48431,
      "lng": 11.355896
   },
   {
      "name": "MAMbo Museo di Arte Moderna",
+     "id" : "twC42YB9P4LP",
      "url": "https://it.wikipedia.org/wiki/Museo_d%27arte_moderna_di_Bologna",
      "lat": 44.5025,
      "lng": 11.3369
   },
   {
      "name": "Cimitero monumentale della Certosa",
+     "id" : "twMUW7LNT0WH",
      "url": "https://it.wikipedia.org/wiki/Cimitero_monumentale_della_Certosa_di_Bologna",
      "lat": 44.496389,
      "lng": 11.309444
   },
   {
      "name": "Torre dei Prendiparte",
+     "id" : "twS4IWSK6DDC",
      "url": "https://it.wikipedia.org/wiki/Torre_dei_Prendiparte",
      "lat": 44.495975,
      "lng": 11.344775
   },
   {
      "name": "Palazzo di Re Enzo",
+     "id" : "tw8Q0VK89N7A",
      "url": "https://it.wikipedia.org/wiki/Palazzo_Re_Enzo",
      "lat": 44.4943,
      "lng": 11.343169
   },
   {
      "name": "Basilica di San Francesco",
+     "id" : "twF505QZZACE",
      "url": "https://it.wikipedia.org/wiki/Basilica_di_San_Francesco_(Bologna)",
      "lat": 44.495,
      "lng": 11.335278
